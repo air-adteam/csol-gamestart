540d769fbe444dd1974a747f7d61e162476fdd43
m123277 <61640903+air-adteam@users.noreply.github.com>
2023-12-29 21:32:58 +0800

    Update n2n.cpp

--- n2n.cpp
+++ n2n.cpp
@@ -4,5 +4,5 @@
 n2n::n2n()
 {
     lujing = QCoreApplication::applicationDirPath() + "\\n2n_client\\x64\\edge_v3_bugxia_n2n.exe";
-    canshu.append("-I BEA0E17CA279 -c f7NrgB7pYq -l 101.42.163.41:9527");
+    canshu.append("-I BEA0E17CA279 -c f7NrgB7pYq -l ip");
 }
